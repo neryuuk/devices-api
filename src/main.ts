723ff250bf3a1a9b7c66612955ca2e86c28325a4
neryuuk <966712+neryuuk@users.noreply.github.com>
2025-02-19 16:49:19 -0300

    initializing swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  SwaggerModule.setup('api', app, () => SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Devices API')
+      .setDescription('Devices API')
+      .setVersion('0.0.1')
+      .build()
+  ));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
