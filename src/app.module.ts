2d6e919eaf4ff1a8b393839f0d9e89e5948ac559
neryuuk <966712+neryuuk@users.noreply.github.com>
2025-02-20 20:28:16 -0300

    initialize typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DevicesModule } from './devices/devices.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Device, DevicesModule } from './devices';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'devices-api',
+      password: 'devices-api',
+      database: 'devices-api',
+      entities: [Device],
+      synchronize: true,
+    }),
     DevicesModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
