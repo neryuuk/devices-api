6d12c9e1590872a95601e7c28055e4c54a98b54c
neryuuk <966712+neryuuk@users.noreply.github.com>
2025-02-21 21:36:03 -0300

    logger parsing

--- src/custom.logger.ts
+++ src/custom.logger.ts
@@ -1,7 +1,11 @@
 import { ConsoleLogger } from '@nestjs/common'
 
 export class CustomLogger extends ConsoleLogger {
-  protected getTimestamp(): string {
+  protected override formatPid(pid: number): string {
+    return `[Nest] ${pid} - `
+  }
+
+  protected override getTimestamp(): string {
     return new Date().toISOString().split('T').join(' ')
   }
 }
