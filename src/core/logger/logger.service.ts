5498d09526a38e8a265ac67fa82e250bd87e4447
neryuuk <966712+neryuuk@users.noreply.github.com>
2025-02-23 13:13:57 -0300

    log formatting

--- src/core/logger/logger.service.ts
+++ src/core/logger/logger.service.ts
@@ -1,11 +1,15 @@
-import { ConsoleLogger } from '@nestjs/common'
+import { ConsoleLogger, LogLevel } from '@nestjs/common';
 
 export class LoggerService extends ConsoleLogger {
   protected override formatPid(pid: number): string {
-    return `[Nest] ${pid} - `
+    return pid.toString()
   }
 
   protected override getTimestamp(): string {
     return new Date().toISOString().split('T').join(' ')
+  }
+
+  protected override formatMessage(logLevel: LogLevel, message: unknown, pidMessage: string, formattedLogLevel: string, contextMessage: string, timestampDiff: string): string {
+    return `[${this.colorize(pidMessage, logLevel)}|${this.colorize(formattedLogLevel, logLevel)}] ${this.getTimestamp()} ${contextMessage}${this.stringifyMessage(message, logLevel)}${timestampDiff}\n`;
   }
 }
