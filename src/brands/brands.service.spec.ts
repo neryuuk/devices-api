2e5a4c4975ccec8438b719b4bd1ba156e6af3df7
neryuuk <966712+neryuuk@users.noreply.github.com>
2025-02-24 00:53:41 -0300

    services coverage

--- src/brands/brands.service.spec.ts
+++ src/brands/brands.service.spec.ts
@@ -1,22 +1,52 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { CoreModule } from '../core'
-import { Brand } from './brand.entity'
-import { BrandsService } from './brands.service'
+import { Test, TestingModule } from '@nestjs/testing';
+import { brandMock } from './brand.mock';
+import { BrandsService } from './brands.service';
 
 describe('BrandsService', () => {
   let service: BrandsService
+  const dataSource = {
+    createQueryBuilder: jest.fn(() => {
+      return { insert: jest.fn(() => {
+        return { values: jest.fn(() => {
+          return { returning: jest.fn(() => {
+            return { execute: jest.fn(() => Promise.resolve({ raw: brandMock })) }
+          }) }
+        }) }
+      }) }
+    }),
+    findOneBy: jest.fn(() => brandMock[1]),
+    find: jest.fn(() => brandMock),
+    delete: jest.fn(() => Promise.resolve({ affected: 1 })),
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([Brand]), CoreModule],
-      providers: [BrandsService],
+      providers: [
+        BrandsService,
+        { provide: 'BrandRepository', useValue: dataSource },
+      ],
     }).compile()
 
-    service = module.get<BrandsService>(BrandsService)
+    service = module.get(BrandsService)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should return created entity', async () => {
+    expect(await service.create(brandMock[0])).toBe(brandMock[0])
+  })
+
+  it('should return entity by id', async () => {
+    expect(await service.findOne(brandMock[1].id)).toBe(brandMock[1])
+  })
+
+  it('should return all entities', async () => {
+    expect(await service.findAll()).toBe(brandMock)
+  })
+
+  it('should remove entity by id', async () => {
+    expect(await service.remove()).toBe(true)
   })
 })
--- src/devices/devices.service.spec.ts
+++ src/devices/devices.service.spec.ts
@@ -1,22 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { CoreModule } from '../core'
-import { Device } from './device.entity'
+import { deviceMock } from './device.mock'
 import { DevicesService } from './devices.service'
 
 describe('DevicesService', () => {
   let service: DevicesService
+  const dataSource = {
+    findOneBy: jest.fn(() => Promise.resolve(deviceMock[0])),
+    findBy: jest.fn(() => deviceMock),
+    softDelete: jest.fn(() => Promise.resolve({ affected: 1 })),
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([Device]), CoreModule],
-      providers: [DevicesService],
+      providers: [
+        DevicesService,
+        { provide: 'DeviceRepository', useValue: dataSource },
+      ],
     }).compile()
 
-    service = module.get<DevicesService>(DevicesService)
+    service = module.get(DevicesService)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should return entity by id', async () => {
+    expect(await service.findOne(deviceMock[0].id)).toBe(deviceMock[0])
+  })
+
+  it('should return all entities', async () => {
+    expect(await service.findAll()).toBe(deviceMock)
+  })
+
+  it('should remove entity by id', async () => {
+    expect(await service.remove(deviceMock[0].id)).toBe(true)
   })
 })
