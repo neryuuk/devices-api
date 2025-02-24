57f8d534afc5e21d2fbd895d1a8ba9bd49848fda
neryuuk <966712+neryuuk@users.noreply.github.com>
2025-02-23 23:17:57 -0300

    device controller coverage

--- src/devices/devices.controller.spec.ts
+++ src/devices/devices.controller.spec.ts
@@ -1,24 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { CoreModule } from '../core'
-import { Device } from './device.entity'
+import { deviceMock } from './device.mock'
 import { DevicesController } from './devices.controller'
 import { DevicesService } from './devices.service'
 
+const mockService = {
+  create: jest.fn().mockReturnValue(deviceMock[0]),
+  findOne: jest.fn().mockReturnValue(deviceMock[1]),
+  findAll: jest.fn().mockReturnValue(deviceMock),
+  update: jest.fn().mockReturnValue(deviceMock[0]),
+  remove: jest.fn().mockReturnValue(true),
+}
+
 describe('DevicesController', () => {
   let controller: DevicesController
+  let service: DevicesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([Device]), CoreModule],
       controllers: [DevicesController],
-      providers: [DevicesService],
+      providers: [{ provide: DevicesService, useValue: mockService }],
     }).compile()
 
-    controller = module.get<DevicesController>(DevicesController)
+    controller = module.get(DevicesController)
+    service = module.get(DevicesService)
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  it('should return created entity', async () => {
+    const result = await controller.create(deviceMock[0])
+    expect(result).toBe(deviceMock[0])
+  })
+
+  it('should fail to create entity', () => {
+    const payload = { ...deviceMock[0] }
+    delete payload.id
+    delete payload.created_at
+    delete payload.deleted_at
+    delete payload.brand_id
+    expect(controller.create(payload)).rejects
+  })
+
+  it('should return entity by id', async () => {
+    const result = await controller.findOne(deviceMock[1].id)
+    expect(result).toBe(deviceMock[1])
+  })
+
+  it('should failt to return entity by id', () => {
+    expect(controller.findOne('a')).rejects
+  })
+
+  it('should return all entities', async () => {
+    expect(await controller.findAll()).toBe(deviceMock)
+  })
+
+  it('should return no entities', async () => {
+    jest.spyOn(service, 'findAll').mockReturnValue([])
+    expect(await controller.findAll()).toHaveLength(0)
+  })
+
+  it('should remove entity by id', async () => {
+    expect(await controller.remove()).toBe(true)
+  })
+
+  it('should fail to remove entity by id', async () => {
+    jest.spyOn(service, 'remove').mockReturnValue(false)
+    expect(await controller.remove()).toBe(false)
   })
 })
